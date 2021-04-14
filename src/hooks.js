b6de0a46a0e2f4afce6907dfda42708c2c7e3af2
arpit <arpit2.sharma@paytmmoney.com>
2021-04-14 16:55:20 +0530

    fixed test

--- src/hooks.js
+++ src/hooks.js
@@ -6,15 +6,9 @@ const width = window.innerWidth;
 
 
 const CONSTRAINTS = {
-	BASIC: {audio:false,  video: true},
-    FRONT_CAMERA: { audio: false, video: { facingMode: { exact: 'user' } } },
-  	BACK_CAMERA: {
-		audio: false,
-		video: {
-		facingMode: { exact: 'environment' },
-		width: { min: 640, ideal: 1920, max: 1920 },
-		height: { min: 400, ideal: 1080 },
-		},
+	BASIC: {audio:false,  video: { aspectRatio: window.innerHeight / window.innerWidth } },
+    FRONT_CAMERA: (deviceID) => {
+		return { video: { deviceId: deviceID } }
 	},
 }
 let initialLoadDone = false;
@@ -52,6 +46,7 @@ export function useCameraStream(cameraStatus, backCamera, onChange) {
 	const [switchingCamera, setSwitchingCamera] = useState(false);
 	const [firstCameraStream, setFirstCameraStream] = useState(null);
 	const [backCameraStream, setBackCameraStream] = useState(null);
+	const [ad, setAd] = useState([]);
 
 	const videoRef = useRef();
 	const testVideoRef = useRef();
@@ -70,30 +65,48 @@ export function useCameraStream(cameraStatus, backCamera, onChange) {
 	}
 
 	const init = async () => {
-		try {
-			// videoRef.current.hidden = true;
-			const stream = await openMediaDevices(frontCamera ? CONSTRAINTS.FRONT_CAMERA : CONSTRAINTS.BACK_CAMERA);
-			console.log('Got MediaStream:', stream);
-			createVideoTag(stream);
-			// initialLoadDone = true;
-			// if(frontCamera){
-			// 	setFirstCameraStream(stream);
-			// }else{
-			// 	setBackCameraStream(stream);
-			// }
-			// setMediaStream(stream);
-			cameraStream = stream;
-			// videoRef.current.srcObject = cameraStream;
-		} catch(error) {
-			console.error('Error accessing media devices.', error);
-			console.log('OverconstrainedError', error.name);
-			if(error.name === 'OverconstrainedError'){
-				const stream = await openMediaDevices(CONSTRAINTS.BASIC);
-				setMediaStream(stream);
+		const enumeratorPromise = await navigator.mediaDevices.enumerateDevices();
+		console.log('enumeratorPromise', enumeratorPromise)
+		const test = {};
+		enumeratorPromise.forEach((device) => {
+			if(device.label.includes("front")){
+				test['front'] = device.deviceId;
+			}else if(device.label.includes("back")){
+				test['back'] = device.deviceId;
+			}
+		})
+		console.log(test);
+		setAd(test);
+		
+	}
+
+	useEffect(async () => {
+		if(ad){
+			try {
+				// videoRef.current.hidden = true;
+				const stream = await openMediaDevices(CONSTRAINTS.FRONT_CAMERA(frontCamera ? ad['front'] : ad['back']));
+				console.log('Got MediaStream:', stream);
 				createVideoTag(stream);
+				// initialLoadDone = true;
+				// if(frontCamera){
+				// 	setFirstCameraStream(stream);
+				// }else{
+				// 	setBackCameraStream(stream);
+				// }
+				// setMediaStream(stream);
+				cameraStream = stream;
+				// videoRef.current.srcObject = cameraStream;
+			} catch(error) {
+				console.error('Error accessing media devices.', error);
+				console.log('OverconstrainedError', error.name);
+				if(error.name === 'OverconstrainedError'){
+					const stream = await openMediaDevices(CONSTRAINTS.BASIC);
+					setMediaStream(stream);
+					createVideoTag(stream);
+				}
 			}
 		}
-	}
+	}, [ad])
 
 	const updateMediaDevices = async () => {
 		try {
@@ -106,16 +119,38 @@ export function useCameraStream(cameraStatus, backCamera, onChange) {
 			// videoRef.current.hidden = true;
 			// tracks.forEach(track => track.stop());
 			// videoRef.current.hidden = true;
-			cameraStream.getTracks().forEach(track => track.stop());
-			const stream = await openMediaDevices(frontCamera ? CONSTRAINTS.FRONT_CAMERA : CONSTRAINTS.BACK_CAMERA);
-			createVideoTag(stream);
+			window.cameraStream = cameraStream;
+			// console.log(cameraStream)
+			// console.log('asdas', cameraStream.getVideoTracks()[0].applyConstraints())
+
+			// cameraStream.getVideoTracks()[0].applyConstraints(frontCamera ? CONSTRAINTS.FRONT_CAMERA : CONSTRAINTS.BACK_CAMERA).then(() => {
+			// 	console.log('allalala switch done');
+			//   })
+			//   .catch(e => {
+			// 	console.log('allalala switch not done');
+			// 	// The constraints could not be satisfied by the available devices.
+			//   });
+			// const stream = await openMediaDevices(CONSTRAINTS.BACK_CAMERA);
+			// cameraStream.addTrack(stream);
+			// createVideoTag(stream);
 
 			// videoRef.current.load();
 			// videoRef.current.play();
 
-			cameraStream = stream;
-			console.log('Got MediaStream:', stream);
+			// cameraStream = stream;
+			// console.log('Got MediaStream:', stream);
 			// videoRef.current.srcObject = cameraStream;
+			const stream = await openMediaDevices(CONSTRAINTS.FRONT_CAMERA(frontCamera ? ad['front'] : ad['back']));
+				console.log('Got MediaStream:', stream);
+				createVideoTag(stream);
+				// initialLoadDone = true;
+				// if(frontCamera){
+				// 	setFirstCameraStream(stream);
+				// }else{
+				// 	setBackCameraStream(stream);
+				// }
+				// setMediaStream(stream);
+				cameraStream = stream;
 		} catch(error) {
 			console.log(error);
 		}
